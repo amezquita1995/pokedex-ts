03bf9cd4ef6445ba87a821ccf2db6af11119f8d9
Yeferson Gaitan Gomez <yefri.gaitan97@gmail.com>
2026-06-08 18:18:23 -0500

    in-memory repository

--- src/services/PokemonService.ts
+++ src/services/PokemonService.ts
@@ -1,28 +1,29 @@
-import { PokemonRepository } from '../repositories/PokemonRepository';
 import { Pokemon } from '../models/Pokemon';
 
 export class PokemonService {
+    private pokemonList: Pokemon[] = [];
 
-    constructor(private pokemonRepository: PokemonRepository) {}
+    addPokemon(pokemon: Pokemon) {
+        console.log(`Adding pokemon: ${pokemon.name}`);
+        this.pokemonList.push(pokemon);
+    }
 
     getAverageLevel(): number {
-        const pokemonList = this.pokemonRepository.getPokemonList();
-        if (pokemonList.length === 0) {
+        if (this.pokemonList.length === 0) {
             return 0;
         }
-        const totalLevel = pokemonList.reduce(
+        const totalLevel = this.pokemonList.reduce(
             (sum, pokemon) => sum + pokemon.level,
             0
         );
-        return totalLevel / pokemonList.length;
-        }
+        return totalLevel / this.pokemonList.length;
+    }
 
     getFastestPokemon(): Pokemon | null {
-        const pokemonList = this.pokemonRepository.getPokemonList();
-        if (pokemonList.length === 0) {
+        if (this.pokemonList.length === 0) {
             return null;
         }
-        const fastestPokemon = pokemonList.reduce(
+        const fastestPokemon = this.pokemonList.reduce(
             (currentFastest, pokemon) => {
                 if (pokemon.stats.speed > currentFastest.stats.speed) {
                     return pokemon;
